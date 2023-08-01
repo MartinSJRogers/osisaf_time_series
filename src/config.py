359e1443d54fcfc9933e0ba425965e63be84628c
Martin Rogers <marrog@bslws04.nerc-bas.ac.uk>
2023-08-01 16:22:17 +0100

    Changes made to make this an osisaf data extraction only branch, not visualisation

--- src/config.py
+++ src/config.py
@@ -16,12 +16,9 @@ SOURCE_CRS = "epsg:4326"  # reference system of the caribou tracking data
 INTERP_FREQ = "6H"  # interpolation frequency for tracking data
 TIME_COL = "FixDateTime"  # time column name
 
-
-
 # path to downloaded osisaf data- will need to change this to osisaf/south/sicona
 PATH_TO_OSISAF = "/data/hpcdata/users/jambyr/icenet/replacement/data/osisaf/south/siconca"
-#PATH_TO_ICENET = PROJECT_PATH + "/forecasts/caribou_2023_north.nc"
-#PATH_TO_AMSR2 = PROJECT_PATH + "/data/amsr2"
+PATH_TO_AMSR2 = PROJECT_PATH + ""
 
 TEMP_IMG_FOLDER = PROJECT_PATH + "/TEMP_IMG_FOLDER"
 VIS_SAVE_FOLDER = PROJECT_PATH + "/visualisations"
@@ -34,18 +31,19 @@ SIC_PLOTS_FOLDER = PROJECT_PATH + "/plots/sic_plots"
 ### BOUNDS OF PLOTTING DATA ###
 ###############################
 
-#changing to be relevant coords for North pole
+#Unhash to use coords for Northern hemisphere
 """
 central_lat = 68
 central_lon = -111
 lat_span = 3  # degrees
 lon_span = 12  # degrees
 """
+
+#Southern hemisphere coords
 central_lat = -90
 central_lon = 0
 lat_span = 100  # degrees
 lon_span = 78  # degrees
-
 
 # Determine crop corners
 lon_left = central_lon - lon_span / 2
@@ -53,61 +51,18 @@ lon_right = central_lon + lon_span / 2
 lat_upper = central_lat + lat_span / 2
 lat_lower = central_lat - lat_span / 2
 
-lats = [lat_upper, lat_upper, lat_lower, lat_lower]
-lons = [lon_left, lon_right, lon_left, lon_right]
-
-# Transform from lat/lon to EASE2 (EPSG:6932)
-transformer = Transformer.from_crs('epsg:4326', 'epsg:3408')
-
-x, y = transformer.transform(lats, lons)
-x = [np.min(x), np.max(x)]  # ascending
-y = [np.max(y), np.min(y)]  # descending
-
-TRANSFORM = ccrs.LambertAzimuthalEqualArea(0, 90)
-PROJECTION = ccrs.LambertAzimuthalEqualArea(central_lon, 90)
-PLOT_EXTENT = [lon_left, lon_right, lat_lower, lat_upper]
-
-# c_map = mpl.cm.get_cmap("jet").copy()
-c_map = mpl.cm.get_cmap("Blues_r").copy()
-# c_map = mpl.cm.get_cmap("Blues_r").copy()
-bg_map = cimgt.Stamen('terrain-background')
-bg_map = None
-#land_gdf = gpd.read_file(PROJECT_PATH + "/data/coronation_gulf_land_poly.geojson")
-fill_land = True
-caribou_colour = "red"
-
-osisaf_plot_config = {
-    "caribou_colour": caribou_colour,
-    "c_map": c_map,
-    "bg_map": bg_map,
-    "fill_land": fill_land,
-    "plot_extent": [lon_left, lon_right, lat_lower, lat_upper],
-    "crop_x": x,
-    "crop_y": y,
-    "central_lon": central_lon,
-    "full_name": "OSI-SAF",
-    "save_name": "osisaf",
-    "vmin": 0,
-    "vmax": 100,
-    # "levels": [0, 10, 20, 30, 40, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
-}
-
-north_plot_config = {
+north_hem_config = {
     "top": 50,
     "bottom" : 325,
     "left" : 30,
     "right" : 330,
-    "PATH_TO_OSISAF" : "/data/hpcdata/users/jambyr/icenet/replacement/data/osisaf/north/siconca",
-    "plotting_latitude" : 90,
-    "plot_title" : "Daily Arctic sea ice extent"
+    "PATH_TO_OSISAF" : "",
 }
 
-south_plot_config = {
+south_hem_config = {
     "top": 55,
-    "bottom" : 380,
-    "left" : 60,
-    "right" : 385,
-    "PATH_TO_OSISAF" : "/data/hpcdata/users/jambyr/icenet/replacement/data/osisaf/south/siconca",
-    "plotting_latitude" : -90, 
-    "plot_title" : "Daily Antarctic sea ice extent"
+    "bottom" : 370,
+    "left" : 70,
+    "right" : 375,
+    "PATH_TO_OSISAF" : "",
 }
--- src/data/.ipynb_checkpoints/load-checkpoint.py
+++ src/data/.ipynb_checkpoints/load-checkpoint.py
@@ -11,35 +11,13 @@ sys.path.append("../../")
 from src import config
 
 
-def read_csv_as_datetime_geometry(path_to_csv: str,
-                                  source_crs: str = "epsg:4326") -> object:
-    """
-    Load caribou tracking data and convert to datetime indexed geopandas dataframe
-    :param path_to_csv: path to the tracking data csv file
-    :param source_crs: reference system of the tracking dataset
-    :return: datetime indexed geopandas dataframe
-    """
-    # read data with datetime column
-    raw_df = pd.read_csv(path_to_csv, index_col=0, parse_dates=["FixDateTime"])
-
-    # convert datetime to index
-    raw_df["t"] = pd.to_datetime(raw_df["FixDateTime"], format="%Y-%m-%d %H:%M")
-    raw_df = raw_df.set_index("t")  # .tz_localize("UTC")
-
-    # convert long lat to geometry points
-    raw_gdf = gpd.GeoDataFrame(raw_df.drop(["Longitude", "Latitude", "FixDateTime"], axis=1), crs=source_crs,
-                               geometry=[Point(xy) for xy in zip(raw_df.Longitude, raw_df.Latitude)])
-
-    return raw_gdf
-
-
 def load_osisaf(path_to_data: str,
                 date: str) -> object:
     """
 
     :param path_to_data: path to osisaf data folder
     :param date: date in
-    :return:
+    :return: xarray Dataset object
     """
 
     file_path = f"{path_to_data}/{date.year}.nc"
@@ -47,13 +25,12 @@ def load_osisaf(path_to_data: str,
 
     # day of year indexing
     doy = date.day_of_year - 1
+    #subset sic for that day
     sic_data = sic_data["ice_conc"].sel(time=date.strftime("%Y-%m-%d"))
-    
 
-    #mask_array = np.load(config.LAND_MASK_PATH)
-    #sic_data = sic_data.where(mask_array == 0)
+    #assign coords
     sic_data = sic_data.assign_coords(xc=sic_data.xc * 1e3, yc=sic_data.yc * 1e3)
-
+    #convert sic to [0,100]
     sic_data = sic_data * 100
 
     # assign attributes to match direct download
@@ -91,68 +68,8 @@ def load_amsr2(path_to_data: str,
     return sic_data
 
 
-def load_osisaf_year(path_to_data: str) -> object:
-    """
-
-    :param path_to_data:
-    :return:
-    """
-    # load osisaf xarray and mask
-    sic_data = xr.open_mfdataset(path_to_data)
-    land_mask = np.load(config.LAND_MASK_PATH)
-
-    # apply mask
-    sic_data = sic_data.where(land_mask == 0)
-
-    # convert coordinates
-    sic_data = sic_data.assign_coords(xc=sic_data.xc * 1e3, yc=sic_data.yc * 1e3)
-    return sic_data
 
 
-def load_icenet(xr_forecast: object,
-                year: int,
-                season: str,
-                date: str) -> object:
-    """
-
-    :param xr_forecast:
-    :param year:
-    :param season:
-    :param date:
-    :return:
-    """
-    if season == "spring":
-        start_date = pd.Timestamp(f'{year}-03-01')
-    elif season == "autumn":
-        start_date = pd.Timestamp(f'{year}-10-01')
-    else:
-        print(f"Input invalid season <{season}>, should be either spring or autumn")
-
-    leadtime = (date - start_date).days
-
-    icenet_pred = xr_forecast["sic_mean"].sel(time=start_date.strftime("%Y-%m-%d")).sel(leadtime=leadtime)
-    #TODO: NEED TO CHANGE!
-    # How to mask data properly? For now mask zero values
-    icenet_pred = icenet_pred.where(icenet_pred != 0)
-    icenet_pred = icenet_pred.assign_coords(xc=icenet_pred.xc * 1e3, yc=icenet_pred.yc * 1e3)
-
-    return icenet_pred
-
-
-# def load_modis(date, plot_extent):
-#
-#     create_modis_xml(date)
-#     options_list = [
-#         "-of GTiff",
-#         "-projwin $plot_extent[0] $plot_extent[3] $plot_extent[1] $plot_extent[2]"
-#     ]
-#     options_string = " ".join(options_list)
-#     out_ds = gdal.Translate("bands.xml", f"modis_{date}.tiff", options=options_string)
-#     #im = gdal.Open(f"modis_{date}.tiff")
-#     arr = out_ds.ReadAsArray()
-#     modis_xr = build_modis_xarray(arr, plot_extent)
-#
-#     return modis_xr
 
 
 
